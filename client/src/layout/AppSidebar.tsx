e7adc57f1e506f68719e74a60f825490bf45d290
osamaqaseem39 <osama.qa.39@gmail.com>
2025-07-24 21:55:20 +0500

    Initial commit

--- client/src/layout/AppSidebar.tsx
+++ client/src/layout/AppSidebar.tsx
@@ -30,6 +30,8 @@ const navItems: NavItem[] = [
     subItems: [
       { name: "All Products", path: "/products" },
       { name: "Add Product", path: "/products/add" },
+      { name: "Categories", path: "/categories" },
+      { name: "Brands", path: "/brands" },
     ],
   },
   {
