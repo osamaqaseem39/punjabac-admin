5b7f2662ded7b6976a67c38c560dbc0549a4305f
osamaqaseem39 <osama.qa.39@gmail.com>
2025-07-28 20:52:58 +0500

    How it Works fix

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3001',
+  'http://localhost:3000',
   'https://punjabac-admim.vercel.app',
   'https://punjabac-admin-4nq8.vercel.app',
   'https://punjabac.osamaqaseem.online',
